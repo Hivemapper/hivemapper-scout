4871e7942f6796e2ed4fe391e2667dfee01f53a3
Silvio <silvio@hivemapper.com>
2023-10-30 16:19:33 -0500

    Wrap design system

--- src/components/Badge/Badge.tsx
+++ src/components/Badge/Badge.tsx
@@ -2,6 +2,7 @@ import * as React from "react";
 import { cva, type VariantProps } from "class-variance-authority";
 
 import { cn } from "@utils/helpers";
+import classNames from "classnames";
 
 const badgeVariants = cva(
   "inline-flex items-center rounded-full border px-2.5 py-0.5 text-xs font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2",
@@ -25,12 +26,24 @@ const badgeVariants = cva(
 
 export interface BadgeProps
   extends React.HTMLAttributes<HTMLDivElement>,
-    VariantProps<typeof badgeVariants> {}
+    VariantProps<typeof badgeVariants> {
+  stripTailwindClasses?: boolean;
+}
 
-function Badge({ className, variant, ...props }: BadgeProps) {
-  return (
-    <div className={cn(badgeVariants({ variant }), className)} {...props} />
+function Badge({
+  className,
+  variant,
+  stripTailwindClasses,
+  ...props
+}: BadgeProps) {
+  const badgeClasses = classNames(
+    {
+      [cn(badgeVariants({ variant }), className)]: !stripTailwindClasses,
+    },
+    "hm-badge"
   );
+
+  return <div className={badgeClasses} {...props} />;
 }
 
 export { Badge, badgeVariants };
