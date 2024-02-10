853e329764e147876710814a00af8fc2d2941319
siiiiilvio <silvio@hivemapper.com>
2024-02-10 12:28:37 -0600

    Change moduleResolution

--- src/utils/h3.ts
+++ src/utils/h3.ts
@@ -1,6 +1,6 @@
 import * as turf from '@turf/turf';
 import { GeoJSONPolygon } from 'types/geojson';
-import { latLngToCell, gridDisk, cellToBoundary} from '@lib/h3';
+import { latLngToCell, gridDisk, cellToBoundary} from 'h3-js';
 
 function findTouchedH3Hexagons(lon: number, lat: number, radius: number): string[] {
     const resolution = 11;
